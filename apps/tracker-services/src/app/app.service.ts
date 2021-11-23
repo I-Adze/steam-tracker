ea026f7ef538dac17b889f85de2250937b8cd932
I-Adze <adam.ward1202@gmail.com>
2021-11-23 22:43:22 +0000

    Adds list display of API data

--- apps/tracker-services/src/app/app.service.ts
+++ apps/tracker-services/src/app/app.service.ts
@@ -1,15 +1,22 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
+import { SteamApp } from '@tracker/shared/core';
+
+interface GetAppListRes {
+  applist: {
+    apps: SteamApp[];
+  };
+}
 
 @Injectable()
 export class AppService {
   constructor(private httpService: HttpService) {}
   getData() {
     return this.httpService
-      .get(
+      .get<GetAppListRes>(
         'http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json'
       )
-      .pipe(map((res) => ({ ...res, data: res.data.appList.apps })));
+      .pipe(map((res) => res.data.applist.apps));
   }
 }
