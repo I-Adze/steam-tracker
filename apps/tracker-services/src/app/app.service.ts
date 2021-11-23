91b3199e324adaa6c092bc045eb07b164c922ae7
I-Adze <adam.ward1202@gmail.com>
2021-11-23 21:13:59 +0000

    Adds basic Steam API call

--- apps/tracker-services/src/app/app.service.ts
+++ apps/tracker-services/src/app/app.service.ts
@@ -1,8 +1,15 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to tracker-services!' };
+  constructor(private httpService: HttpService) {}
+  getData() {
+    return this.httpService
+      .get(
+        'http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json'
+      )
+      .pipe(map((res) => ({ ...res, data: res.data.appList.apps })));
   }
 }
